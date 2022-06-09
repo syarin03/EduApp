0faa5e2e23212b74afe253396eebe2a2db476860
syarin03 <gami5507@gmail.com>
2022-06-09 14:27:48 +0900

    06081427

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 form_class = uic.loadUiType("main.ui")[0]
+job = ""
 
 
 class WindowClass(QMainWindow, form_class):
@@ -16,10 +17,19 @@ class WindowClass(QMainWindow, form_class):
 
     def gotosignin_click(self):
         self.stackedWidget.setCurrentIndex(1)
+        if self.inchoose_s.isChecked(): job = "stu"
+        elif self.inchoose_t.isChecked(): job = "tea"
+        self.signinbtn.clicked.connect(self.login_click)
 
     def gotosignup_click(self):
         self.stackedWidget.setCurrentIndex(2)
 
+    def login_click(self):
+        if job == "stu":
+            print(job)
+            self.stackedWidget.setCurrentIndex(3)
+        elif job == "tea":
+            print(job)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
